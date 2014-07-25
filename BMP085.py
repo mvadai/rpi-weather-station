610f17b98970023fb0f7c6b58803999177c0ca38
Mihaly Vadai <vadaim@gmail.com>
2014-07-25 12:31:02 +0100

    Update BMP085.py

--- BMP085.py
+++ BMP085.py
@@ -12,17 +12,21 @@ class BMP085:
         self.altitude = altitude;
 
     def update(self):
-        while True:
+        cnt = 0
+        while cnt < 5:
             output = subprocess.check_output(["./sensors/MV_bmp085", str(self.altitude)])
             data = re.search("Temperature:\s+([0-9.]+)", output)
             if not data:
                 time.sleep(1)
+                cnt += 1
+                self.temp = -1.0
+                self.pressure = 0
+                self.pressure_sea = 0
                 continue
             else:
+                self.temp = float(data.group(1))
+                data = re.search("Pressure:\s+([0-9]+)", output)
+                self.pressure = int(data.group(1))
+                data = re.search("level:\s+([0-9]+)", output)
+                self.pressure_sea = int(data.group(1))
                 break
-
-        self.temp = float(data.group(1))
-        data = re.search("Pressure:\s+([0-9]+)", output)
-        self.pressure = int(data.group(1))
-        data = re.search("level:\s+([0-9]+)", output)
-        self.pressure_sea = int(data.group(1))
