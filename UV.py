465fcbc975ba0cec3c67dbd9e3220d971fce9e9f
Mihaly Vadai <vadaim@gmail.com>
2014-07-26 09:35:15 +0100

    Update UV.py

--- UV.py
+++ UV.py
@@ -10,14 +10,16 @@ import settings
 class UV:
 
     def update(self):
-        while True:
+        cnt = 0
+        while cnt < 5:
             output = subprocess.check_output(["./sensors/gp2y10", "1", "2"])
             output = subprocess.check_output(["./sensors/gp2y10", "0"])
             data = re.search("Voltage:\s+([0-9]+)", output)
             if not data:
                 time.sleep(1)
+                self.uv = -1
+                cnt += 1
                 continue
             else:
+                self.uv = round(0.01*(float(data.group(1)))*5090/1024, 1)
                 break
-
-        self.uv = round(0.01*(float(data.group(1)))*5090/1024, 1)
